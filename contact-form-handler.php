cc672b4b3eaa0c09a28c8610ed2dc6201ca70a0a
dbto1970 <donbartojunior@gmail.com>
2021-12-16 15:28:58 -0500

    updated git links for each project

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -11,7 +11,7 @@
                     "User Email: $visitor_email.\n".
                     "User Message: $message.\n";
 
-    $to = "donbartojunior@gmail.com";
+    $to = "don@donbarto.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to, $email_subject. $email_body, $headers);
